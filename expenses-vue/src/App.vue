e0530a909516315f5ab6c43d16d9d8b282db6c98
brayvasq <brayvasq@gmail.com>
2018-10-28 21:08:55 -0500

    Basic form in vuejs

---
+++ expenses-vue/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <!--<img alt="Vue logo" src="./assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <FormComponent class="component component-one"></FormComponent>
+    </div>
+</template>
+
+<script>
+    //import HelloWorld from './components/HelloWorld.vue'
+    import FormComponent from './components/FormComponent.vue';
+    export default {
+        name: 'app',
+        components: {
+            //HelloWorld
+            FormComponent
+        }
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Pacifico');
+
+    #app {
+        font-family: 'Pacifico', cursive,'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        //margin-top: 60px;
+    }
+
+    body{
+        margin: 0;
+        padding: 0;
+    }
+
+    .component{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+    }
+
+    .component-one{
+        background: linear-gradient(#2ecc71,#34495e);
+    }
+</style>
---
+++ expenses-vue/src/components/FormComponent.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <div class="form-cont">
+            <h1> Add a Expense </h1>
+            <div class="form-page">
+                <input type="text" placeholder="Input your item here" v-model="itemName"/>
+                <input type="number" placeholder="Input the price of item" v-model="itemPrice"/>
+                <input type="submit" @click="saveExpense">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FormComponent',
+        data() {
+            return {
+                itemName: '',
+                itemPrice: ''
+            }
+        },
+        methods: {
+            saveExpense() {
+                console.log(`Item : ${this.itemName}`);
+                console.log(`Price : ${this.itemPrice}`);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .form-page {
+        margin-top: 30%;
+        height: -webkit-fill-available;
+    }
+
+    .form-cont{
+        display: grid;
+    }
+
+    input[type="text"], input[type="number"] {
+        display: block;
+        margin-top: 2%;
+        //margin-left: 10%;
+        padding-top: 5%;
+        padding-bottom: 5%;
+        padding-left: 2%;
+        padding-right: 2%;
+        width: 100%;
+        font-family: 'Pacifico', cursive;
+        font-size: 18px;
+        -webkit-box-shadow: 0px 5px 4px #c0c0c09d;
+        -moz-box-shadow: 0px 5px 4px #c0c0c09d;
+        box-shadow: 0px 5px 4px #c0c0c09d;
+        border:none;
+        outline: none;
+    }
+
+    input[type="submit"], input[type="button"] {
+        margin-top: 10%;
+        padding-top: 2%;
+        padding-bottom: 2%;
+        padding-left: 30%;
+        padding-right: 30%;
+        cursor: pointer;
+        border: solid 3px white;
+        outline: none;
+        font-family: 'Pacifico', cursive;
+        font-size: 18px;
+        //background: linear-gradient(to right,white,white,#34495e) no-repeat ;
+        background: transparent;
+        color: white;
+        border-radius: 15px;
+    }
+    input[type="submit"]:hover, input[type="button"]:hover{
+        background: white;
+        color: #34495e;
+    }
+
+    h1{
+        font-size: 48px;
+        color: white;
+        margin-bottom: -10%;
+    }
+
+</style>
