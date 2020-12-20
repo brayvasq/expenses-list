966cc8ede92a3770e0d502e74ea213b7e95795b0
Brayan Vasquez <brayvasq@gmail.com>
2020-12-20 17:44:40 -0500

    Refactoring: move to react hooks (#4)

--- expenses-react/src/App.js
+++ expenses-react/src/App.js
@@ -4,23 +4,21 @@ import './App.css';
 import FormComponent from './components/FormComponent';
 import ListComponent from './components/ListComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="">
-          <div className="Container Container-one">
-              <div className="Component Component-one">
-                  <FormComponent/>
-              </div>
-          </div>
-          <div className="Container Container-two">
-              <div className="Component">
-                  <ListComponent/>
-              </div>
-          </div>
+const App = () => {
+  return (
+    <div className="">
+      <div className="Container Container-one">
+        <div className="Component Component-one">
+          <FormComponent />
+        </div>
       </div>
-    );
-  }
-}
+      <div className="Container Container-two">
+        <div className="Component">
+          <ListComponent />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- expenses-react/src/components/FormComponent.js
+++ expenses-react/src/components/FormComponent.js
@@ -1,72 +1,43 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './FormComponent.css';
 import axios from 'axios';
 
-/**
- * Componente que funciona como el formulario de creación de gastos
- */
-class FormComponent extends Component{
-    /**
-     * Constructor donde se definen las propiedades del componente.
-     * Aquí se puede definir la lista de estados y enlazar los métodos al componente.
-     * @param props propiedades explícitas del componente
-     */
-    constructor(props){
-        super(props)
-        this.state = {
-            itemName:'',
-            itemPrice: ''
-        };
-        this.addItem = this.addItem.bind(this);
-        this.handleChangeName = this.handleChangeName.bind(this);
-        this.handleChangePrice = this.handleChangePrice.bind(this);
-    }
+const FormComponent = props => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
 
-    /**
-     * Método que actualiza el estado del estado itemName cuando cambia un input
-     * @param event evento ejecutado
-     */
-    handleChangeName(event) {
-        this.setState({itemName: event.target.value});
-    }
-    /**
-     * Método que actualiza el estado del estado itemPrice cuando cambia un input
-     * @param event evento ejecutado
-     */
-    handleChangePrice(event){
-        this.setState({itemPrice: event.target.value});
-    }
+    const handleChangeName = event => setName(event.target.value);
 
-    /**
-     * Método que hace la petición al ws para crear un recurso
-     * @returns {Promise<void>}
-     */
-    async addItem(){
+    const handleChangePrice = event => setPrice(event.target.value);
+
+    const addItem = async () => {
         const data = {
-            item: this.state.itemName,
-            price: this.state.itemPrice
-        };
+            item: name,
+            price
+        }
 
-        const resp = await axios.post('http://localhost:5040/expenses',data);
-        console.log(resp);
+        await axios.post('http://localhost:5040/expenses', data);
     }
-    /**
-     * Método que renderiza el componente, aquí va el template del mismo.
-     * @returns {*} template del componente
-     */
-    render(){
-        return (
-            <div className="form-cont">
-                <h1> Add a Expense </h1>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png" alt=""/>
-                    <div className="form-page">
-                        <input type="text" placeholder="Input your item here" value={this.state.itemName} onChange={this.handleChangeName}/>
-                        <input type="number" placeholder="Input the price of item" value={this.state.itemPrice} onChange={this.handleChangePrice}/>
-                        <input type="submit" value="Send" onClick={this.addItem}/>
-                    </div>
+
+    return (
+        <div className="form-cont">
+            <h1> Add a Expense </h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png" alt="" />
+            <div className="form-page">
+                <input
+                    type="text"
+                    placeholder="Input your item here"
+                    value={name}
+                    onChange={handleChangeName} />
+                <input
+                    type="number"
+                    placeholder="Input the price of item"
+                    value={price}
+                    onChange={handleChangePrice} />
+                <input type="submit" value="Send" onClick={addItem} />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default FormComponent;
