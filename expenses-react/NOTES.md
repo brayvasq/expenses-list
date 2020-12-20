4fa951f4b098cc43aa1c4c9e094154ed421fb19c
brayvasq <brayvasq@gmail.com>
2020-12-20 13:07:47 -0500

    (react) project setup

---
+++ expenses-react/NOTES.md
@@ -0,0 +1,39 @@
+# React Frontend
+ReactJS project to consume an expenses API.
+
+## Setup project
+Create the react project app
+```bash
+npx create-react-app expenses-react
+```
+
+Install `redux`
+```bash
+npm i redux --save-dev
+```
+
+Install `react-redux`
+```bash
+npm i react-redux --save-dev
+```
+
+### Structure project
+```bash
+# Stores directory
+mkdir -p src/js/store
+
+# Reducers directory
+mkdir -p src/js/reducers
+
+# Actions directory
+mkdir -p src/js/actions
+
+# Constants directory
+mkdir -p src/js/constants
+```
+
+
+## References
+- https://www.valentinog.com/blog/redux/
+- https://react-redux.js.org/introduction/quick-start
+- https://www.youtube.com/watch?v=oD3miHerQbY
--- expenses-react/src/App.js
+++ expenses-react/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FormComponent from './components/FormComponent';
-import ListComponent from './components/ListComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="">
-          <div className="Container Container-one">
-              <div className="Component Component-one">
-                  <FormComponent/>
-              </div>
-          </div>
-          <div className="Container Container-two">
-              <div className="Component">
-                  <ListComponent/>
-              </div>
-          </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
